8c323c13e7fedbf8c1ece0d1f893c2dbe07240fc
LordnistLost <lordnsmaus@gmail.com>
2023-09-23 11:22:15 +0200

    skip sets with video. (s.e.-FBL)

--- get_osu_maps.py
+++ get_osu_maps.py
@@ -5,16 +5,18 @@ import os
 import time
 
 count = 0
-sets = 3601
+sets = 3806
 while(count < 10001):
     response = requests.get(f"https://api.chimu.moe/v1/search?amount=100&offset={sets}&min_diff=1&max_diff=8&min_bpm=0&max_bpm=999")
     if not response.status_code == 200:
         print("server problem or something fuck")
-        time.sleep(5)
         sets += 1
         continue
     files = response.json()["data"]
     for osu_set in files:
+        if osu_set["HasVideo"]:
+            print("osu_set had video -> skip")
+            continue
         for osu_map in osu_set["ChildrenBeatmaps"]:
             with open(file=f"data/osumap{osu_map['BeatmapId']}.json", mode="w") as map_file:
                 map_file.write(str(osu_map))
@@ -24,7 +26,6 @@ while(count < 10001):
             if not map_zip_file.status_code == 200:
                 print("Unable to get map_file_zip")
                 os.remove(map_file.name)
-                time.sleep(1)
                 continue
             with zipfile.ZipFile(io.BytesIO(map_zip_file.content), mode="r") as zip:
                 for z in zip.infolist():
