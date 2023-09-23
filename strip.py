77359c07a8ba2646eb953137a852e855f4bb525d
LordnistLost <lordnsmaus@gmail.com>
2023-09-24 01:05:32 +0200

    change csv value to 10 to optimise data. (s.e.-FBL)

--- strip.py
+++ strip.py
@@ -1,10 +1,10 @@
 
 def strip_file():
     with open("woah/properties_old(15).csv", "r") as file:
-        with open("woah/properties_new.csv", "w") as new_file:
+        with open("woah/properties.csv", "w") as new_file:
             new_file.write(file.readline())
             for line in file.readlines()[1:]:
-                if not float(line.split(",")[0]) > 12:
+                if not float(line.split(",")[0]) > 10:
                     new_file.write(line)
         
 if __name__ == "__main__":
