e052b84f8194917791ad136d5d78eece65cdc56f
Darxoon <darxoon@outlook.com>
2023-09-24 09:25:16 +0200

    change return to print in main.py

--- main.py
+++ main.py
@@ -5,19 +5,18 @@ import get_osu_maps, osu_file_filter, ai_generate
 def main():
     osu_map_id = input("Map id: ")
     try:
-        get_osu_maps.get_single_map(map_id=osu_map_id)
+        get_osu_maps.get_single_map(osu_map_id)
         osu_file_filter.filter_files(folder_name="tests", is_training=False)
         csv_refactor.create_csv(is_training=False)
-        with open(file="request.csv", mode="r") as request:
+        with open("request.csv", "r") as request:
             actual_value = request.readlines()[1].split(",")[0]
         prediction = ai_generate.AI_query("request.csv")[0][0]
         result = f"Die vorhergesagte Schwierigkeit ist {str(prediction)}, die eigentliche Schwierigkeit ist {actual_value}."
         print(result)
-        return result
     except (ValueError, UnicodeDecodeError):
-        return "Bitte Versuche eine andere id"
+        print("Bitte Versuche eine andere id")
     except ConnectionError:
-        return "Die API ist zur Zeit offline"
+        print("Die API ist zur Zeit offline")
     
 if __name__ == "__main__":
     main()
